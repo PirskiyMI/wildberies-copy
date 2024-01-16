1bdfa43c6228e36ee4a728a2afd526a2481386b1
Maksim <100757585+Maksim-pp@users.noreply.github.com>
2024-01-16 13:57:58 +0300

    Added a stub to the component for profile-trevel, profile-reviews, profile-purchases, profile-appeals

--- src/pages/profile/profile-reviews/ui/index.tsx
+++ src/pages/profile/profile-reviews/ui/index.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
 
 import styles from './styles.module.scss';
+import { ProfileReorder } from 'src/widgets/profile';
 
 const ProfileReviews: FC = () => {
-   return <div className={styles.reviews}>ProfileReviews</div>;
+   return <div className={styles.reviews}>
+      <div className={`${styles.favorites__container} container`}>
+         <ProfileReorder
+            title="Здесь будут ваши отзывы"
+            text="Помогите другим покупателям сделать выбор — поделитесь мнением о товарах в разделе Покупки"
+         />
+      </div>
+   </div>;
 };
 
 export default ProfileReviews;
