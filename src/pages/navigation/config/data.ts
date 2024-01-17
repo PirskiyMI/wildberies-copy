09e770e543ebe691eacef2359850e99814af2661
Maksim <100757585+Maksim-pp@users.noreply.github.com>
2024-01-17 16:48:22 +0300

    pull

---
+++ src/pages/navigation/config/data.ts
@@ -0,0 +1,28 @@
+import { IMenuList } from 'src/widgets/burger-menu';
+
+export const menuList: IMenuList[] = [
+   {
+      id: "men's clothing",
+      title: 'Мужская одежда',
+      path: '#',
+      icon: 'user-male',
+   },
+   {
+      id: "women's clothing",
+      title: 'Женская одежда',
+      path: '#',
+      icon: 'user-female',
+   },
+   {
+      id: 'jewelery',
+      title: 'Ювелирные изделия',
+      path: '#',
+      icon: 'ring',
+   },
+   {
+      id: 'electronics',
+      title: 'Электроника',
+      path: '#',
+      icon: 'electronics',
+   },
+];
--- src/pages/navigation/ui.tsx
+++ src/pages/navigation/ui.tsx
@@ -1,7 +1,23 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'src/shared';
+import { menuList } from './config';
+import styles from './styles.module.scss'
 
 const Navigation: FC = () => {
-   return <div>BurgerMenu</div>;
+   return <nav className={styles.menu}>
+      <ul className={styles.menu__list}>
+         {  menuList.map((el) => (
+            <li key={el.id} className={styles.menu__item}>
+               <Link to={`/catalog/${el.id}`} className={styles.menu__link}>
+               <Icon icon={el.icon} className={styles.menu__icon}/>
+                  {el.title}
+               </Link>
+            </li>
+         )
+         )}
+      </ul>
+   </nav>;
 };
 
 export default Navigation;
